ab9c14af63f2bee097faf626e491764f13073046
HO PHUC HIEU <hophuchiei135@gmail.com>
2025-01-13 09:29:42 +0700

    chatbot

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/layout/header";
 import Footer from "./components/layout/footer";
-// import Chatbot from "./components/layout/Chatbot";
+import Chatbot from "./components/layout/Chatbot";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
-      {/* <Chatbot></Chatbot> */}
+      <Chatbot></Chatbot>
       <Header></Header>
       <Outlet></Outlet>
       <Footer></Footer>
